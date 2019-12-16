e5880500c113a8bc6f04f8212394b3b61a19e2da
Edwin Wang <lark930@gmail.com>
2019-12-16 19:38:12 +0800

    Add bridge structs

--- plugins/bridge_plugin/include/eosio/bridge_plugin/bridge_plugin.hpp
+++ plugins/bridge_plugin/include/eosio/bridge_plugin/bridge_plugin.hpp
@@ -5,10 +5,12 @@
 #pragma once
 #include <appbase/application.hpp>
 #include <eosio/chain_plugin/chain_plugin.hpp>
+#include <eosio/chain/types.hpp>
 
 namespace eosio {
 
 using namespace appbase;
+using namespace chain;
 
 class bridge_plugin : public appbase::plugin<bridge_plugin> {
 public:
@@ -26,4 +28,32 @@ private:
    std::unique_ptr<class bridge_plugin_impl> my;
 };
 
+   struct bridge_blocks {
+      block_id_type                             id;
+      block_state                               bls;
+   };
+
+   struct bridge_change_schedule {
+      block_id_type                             id;
+      incremental_merkle                        imcre_merkle;
+      std::vector<signed_block_header>          block_headers;
+      std::vector<std::vector<block_id_type>>   block_id_lists;
+      uint8_t                                   status;
+   };
+
+   struct bridge_prove_action {
+      block_id_type                             id;
+      action                                    act;
+      action_receipt                            act_receipt;
+      std::vector<block_id_type>                act_receipt_merkle_paths;
+      incremental_merkle                        imcre_merkle;
+      std::vector<signed_block_header>          block_headers;
+      std::vector<std::vector<block_id_type>>   block_id_lists;
+      uint8_t                                   status;
+   };
+
 }
+
+FC_REFLECT( eosio::bridge_blocks, (id)(bls) )
+FC_REFLECT( eosio::bridge_change_schedule, (id)(imcre_merkle)(block_headers)(block_id_lists)(status) )
+FC_REFLECT( eosio::bridge_prove_action, (id)(act)(act_receipt)(act_receipt_merkle_paths)(imcre_merkle)(block_headers)(block_id_lists)(status) )
